e755cd6918b853b656e2c8c3b20d9e488009ebab
Jules LAURENT <jules.laurent@ynov.com>
2018-11-07 16:17:01 +0100

    create messages model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_145932) do
+ActiveRecord::Schema.define(version: 2018_11_07_151541) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.integer "likes"
+    t.integer "dislikes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
