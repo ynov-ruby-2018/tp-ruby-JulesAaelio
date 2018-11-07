16a35973b0ce09f606ad29d0a7aa5b0e53176e6a
Jules LAURENT <jules.laurent@ynov.com>
2018-11-07 17:22:51 +0100

    add like dislike buttons and behavior

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -20,4 +20,29 @@ class MessageController < ApplicationController
         :content,
     ).merge(user_id: current_user.id)
   end
+
+  def like
+    @message = Message.find(params[:id])
+    if @message.likes == nil
+      @message.update({likes: 1})
+    else
+      @message.update({likes: @message.likes + 1})
+    end
+
+    redirect_to message_index_path
+  end
+
+  def dislike
+    @message = Message.find(params[:id])
+    if @message.dislikes == nil
+      @message.update({dislikes: 1})
+    else
+      @message.update({dislikes: @message.dislikes + 1})
+    end
+
+    redirect_to message_index_path
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "message#index"
   resources :message
+
+  post '/message/:id/like', to: 'message#like', as: 'message_like'
+  post '/message/:id/dislike', to: 'message#dislike', as: 'message_dislike'
 end
