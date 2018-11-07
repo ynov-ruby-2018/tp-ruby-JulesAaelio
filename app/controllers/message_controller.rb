6f37112b0c4f2a1405aaeff93db55e2add00a18e
Jules LAURENT <jules.laurent@ynov.com>
2018-11-07 17:09:50 +0100

    add message creation form

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -4,5 +4,20 @@ class MessageController < ApplicationController
 
   def index
     @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(post_params)
+
+    if @message.save
+      redirect_to message_index_path
+    end
+  end
+
+  def post_params
+    params.require(:message).permit(
+        :content,
+    ).merge(user_id: current_user.id)
   end
 end
