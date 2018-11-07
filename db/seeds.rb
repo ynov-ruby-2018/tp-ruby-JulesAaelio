93fd4c92ff2588d7f611bdb13d5a60628016882a
Jules LAURENT <jules.laurent@ynov.com>
2018-11-07 16:25:40 +0100

    seed messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create({email: "jules.laurent@blabla.com", password: "password", password_confirmation: "password" })
+
+
+Message.create([
+    {content: "Message 1", user: user},
+    {content: "Message 2", user: user},
+    {content: "Message 3", user: user},
+    {content: "Message 4", user: user},
+    {content: "Message 5", user: user}
+               ])
